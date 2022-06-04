5537b4550a7692410112cef5e15afc5deaf7d168
Matanel Mac <mmgordon.matanel@gmail.com>
2022-06-04 17:42:43 +0300

    changed index js

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
 import type {Result} from "./results";
 import {Err, Ok} from "./results";
+import type {ComposerFunction} from "./composers";
+import {composeResult} from "./composers";
 
 
-/*const f = (): Result<number, string> => {
+const f = (): Result<number, string> => {
     const x = 3;
 
     if(x > 5){
         return Ok(x)
     }
     return Err("Blah Blah");
-}*/
-
-type ComposerFunction<T,K> = (ok:(val:T) => void, err: (errVal:K) => void) => unknown;
-
-const composeResult = <T, K>(fn:ComposerFunction<T,K>) => new Promise<Result<T, K>>(res => {
-    const WrappedOk = (value:T) => res(Ok(value));
-    const WrappedErr = (errValue:K) => res(Err(errValue));
-    fn(WrappedOk, WrappedErr);
-})
+}
 
 const fn:ComposerFunction<number, string> = (Ok, Err) => {
     const x = 6;
@@ -31,3 +25,6 @@ const fn:ComposerFunction<number, string> = (Ok, Err) => {
     const result = await composeResult(fn);
     console.log(result.expect("bad shit with result"));
 })()
+
+const result = f();
+console.log(result.expect("error in sync result"));
