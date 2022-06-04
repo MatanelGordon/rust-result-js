1b84e490408cba3d05a49ea242824335cc00188c
Matanel Mac <mmgordon.matanel@gmail.com>
2022-06-04 13:18:28 +0300

    created basic result implementation for ok and err

---
+++ src/ErrorResult.ts
@@ -0,0 +1,43 @@
+import {Result} from "./Result";
+
+export class ErrorResult<T,K> extends Result<T, K>{
+    private readonly errorValue: K;
+
+    constructor(err:K) {
+        super();
+        this.errorValue = err;
+    }
+
+    expect(msg: string): T {
+        throw this.createPanicError(`expect() - ${msg} : ${this.errorValue}`);
+    }
+
+    expectErr(): K {
+        return this.errorValue;
+    }
+
+    map<T1>(): Result<T1, K> {
+        return new ErrorResult<T1,K>(this.errorValue);
+    }
+
+    mapErr<K1>(fn: (err: K) => K1): Result<T, K1> {
+        return new ErrorResult<T,K1>(fn(this.errorValue));
+    }
+
+    unwrap(): T {
+        throw this.createPanicError('unwrap(): called unwrap in an Err');
+    }
+
+    isOk(): boolean {
+        return false;
+    }
+
+    mapOr<T1>(defaultValue: T1): T1 {
+        return defaultValue;
+    }
+
+    unwrapErr(): K {
+        return this.errorValue;
+    }
+
+}
---
+++ src/OkResult.ts
@@ -0,0 +1,45 @@
+import {Result} from "./Result";
+
+export class OkResult<T,K> extends Result<T, K>{
+    private readonly value: T;
+
+    constructor(val:T) {
+        super();
+        this.value = val;
+    }
+
+    isOk(): boolean {
+        return true;
+    }
+
+    mapOr<T1>(_defaultValue: T1, fn: (val: T) => T1): T1 {
+        return fn(this.value)
+    }
+
+    expect(): T {
+        return this.value;
+    }
+
+    expectErr(msg: string): K {
+        throw this.createPanicError(msg);
+    }
+
+    map<T1>(fn: (val: T) => T1): Result<T1, K> {
+        return new OkResult<T1,K>(fn(this.value));
+    }
+
+    mapErr<K1>(): Result<T, K1> {
+        return new OkResult(this.value);
+    }
+
+    unwrap(): T {
+        return this.value
+    }
+
+    unwrapErr():K{
+        throw this.createPanicError('unwrapErr: called unwrapErr in \'Ok\'');
+    }
+
+
+
+}
