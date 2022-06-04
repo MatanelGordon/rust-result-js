3fc8cba28dc10072042dd6f48e57b5505a34c024
Matanel Mac <mmgordon.matanel@gmail.com>
2022-06-04 13:27:02 +0300

    added basic Result usage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-const c:any = {a:1,b:2};
-console.log(c);
-console.log(1234)
+import {ErrorResult} from "./ErrorResult";
+import {OkResult} from "./OkResult";
+import type {Result as Result} from "./Result";
+
+const Ok = <T,K>(val:T) => new OkResult<T,K>(val);
+const Err = <T,K>(err:K) => new ErrorResult<T,K>(err);
+
+const f = (): Result<number, string> => {
+    const x = 3;
+
+    if(x > 5){
+        return Ok(x)
+    }
+    return Err("Blah Blah");
+}
+
+const result = f();
+console.log(result.expect("errored in f"))
