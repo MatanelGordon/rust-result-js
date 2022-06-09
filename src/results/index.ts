70ca52970057382b2f7d2770893ffbddc6eaf20e
Matanel Mac <mmgordon.matanel@gmail.com>
2022-06-10 00:41:40 +0300

    ordered exports

--- src/results/index.ts
+++ src/results/index.ts
@@ -1,7 +1,7 @@
 import { OkResult } from './OkResult';
 import { ErrorResult } from './ErrorResult';
 import {Result} from "./Result";
-export type { Result };
 
+export type { Result };
 export const Ok = <T, K>(val: T):Result<T, K> => new OkResult<T, K>(val);
 export const Err = <T, K>(err: K):Result<T, K> => new ErrorResult<T, K>(err);
