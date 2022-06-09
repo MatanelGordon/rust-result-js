1f46f4b772304b4d1eddff1ab57008316d82543d
Matanel Mac <mmgordon.matanel@gmail.com>
2022-06-10 00:39:29 +0300

    added casting to Ok and Err

--- src/results/index.ts
+++ src/results/index.ts
@@ -1,6 +1,7 @@
 import { OkResult } from './OkResult';
 import { ErrorResult } from './ErrorResult';
-export type { Result } from './Result';
+import {Result} from "./Result";
+export type { Result };
 
-export const Ok = <T, K>(val: T) => new OkResult<T, K>(val);
-export const Err = <T, K>(err: K) => new ErrorResult<T, K>(err);
+export const Ok = <T, K>(val: T):Result<T, K> => new OkResult<T, K>(val);
+export const Err = <T, K>(err: K):Result<T, K> => new ErrorResult<T, K>(err);
